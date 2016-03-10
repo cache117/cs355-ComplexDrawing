863595c747dd7e7767a5a5650547813f059d03ef
Cache Staheli <cache@byu.edu>
2016-03-10 10:45:09 -0700

    Scene files. Matrix.
    Added needed files.

---
+++ src/cs355/view/drawing/util/Matrix.java
@@ -0,0 +1,17 @@
+package cs355.view.drawing.util;
+
+/**
+ * A 4x4 matrix used for 3D Rendering.
+ */
+public class Matrix
+{
+    /**
+     * Multiplies this matrix with another matix.
+     * @param other the other matrix.
+     * @return the resulting matrix from the multiplication.
+     */
+    public Matrix multiply(Matrix other)
+    {
+        return null;
+    }
+}
